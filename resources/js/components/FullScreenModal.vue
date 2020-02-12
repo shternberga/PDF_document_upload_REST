0863395ad7e1d58d854fa740f18987d4acf6d044
Daina Lapsa <dainanana.ercen@gmail.com>
2020-02-12 14:38:32 +0200

    Full Screen Modal

---
+++ resources/js/components/FullScreenModal.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- Modal -->
+        <div class="modal-dialog momodel modal-fluid" role="document">
+            <div class="modal-content ">
+                <button type="button" class="close " data-dismiss="modal" aria-label="Close">
+                    <span style="font-size: 1.3em;" aria-hidden="true">&times;</span>
+                </button>
+                <div class="modal-body">
+                    <embed class="pdf" :src="`uploads/${fullScreenModalDocument.file}`" :alt="documentInModal"/>
+                </div>
+            </div>
+        </div>
+    <!-- Modal -->
+</template>
+
+<script>
+    export default {
+        name: 'FullScreenModal',
+        props: {
+            fullScreenModalDocument: {}
+        },
+        mounted() {
+            console.log('Component mounted (components/FullScreenModal.vue).')
+        },
+        data() {
+            return {}
+        }
+    }
+</script>
